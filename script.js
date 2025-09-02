0246b8e37c9f747f884bb5099ebb91957f88bbca
schierjoao <joaopedroschier69@gmail.com>
2025-09-02 20:54:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+    const meuBotao = document.getElementById('meuBotao');
+    meuBotao.addEventListener('click', () => {
+        window.location.href = 'https://www.kaspersky.com.br/resource-center/definitions/what-is-facial-recognition'; 
+    });
+    const Meubotao = document.getElementById('Meubotao');
+    Meubotao.addEventListener('click', () => {
+        window.location.href = 'https://www.julianetorres.com.br/automacao-residencial-com-alexa-os-beneficios-e-como-comecar/'; // Substitua pela URL do seu site
+    });
+    const botao = document.getElementById('botao');
+    botao.addEventListener('click', () => {
+        window.location.href = 'https://www3.anabb.org.br/biblioteca/noticias/beneficios-e-aplicabilidade-do-pix-no-dia-a-dia'; // Substitua pela URL do seu site
+    });
+    const Botao = document.getElementById('Botao');
+    Botao.addEventListener('click', () => {
+        window.location.href = 'https://usemobile.com.br/smartwatch/'; // Substitua pela URL do seu site
+    });
+        const rE = document.getElementById('rE');
+    rE.addEventListener('click', () => {
+        window.location.href = 'https://www.capsolver.com/pt/blog/All/im-not-a-bot'; // Substitua pela URL do seu site
+    });
+      const VR = document.getElementById('VR');
+    VR.addEventListener('click', () => {
+        window.location.href = 'https://www.xataka.com.br/ciencia/oculos-realidade-aumentada-estavam-fadados-ao-fracasso-zuckerberg-e-rayban-dao-outra-chance-a-eles'; // Substitua pela URL do seu site
+    });
+    const mp = document.getElementById('mp');
+    mp.addEventListener('click', () => {
+        window.location.href = 'https://www.terra.com.br/byte/adeus-ao-mp3-fim-de-uma-era,938536844d812c4e60153656cabc9c3fodyd3ljc.html'
+    });
+    const TV = document.getElementById('TV');
+    TV.addEventListener('click', () => {
+        window.location.href = 'https://oantagonista.com.br/ladooa/entretenimento/a-decadencia-da-tv-a-cabo/'
+    });
+
